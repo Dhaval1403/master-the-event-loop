6431b2a2d07cfa338a52d88ff603d46182340427
Dhaval Mehta <mehtadhaval8228@gmail.com>
2020-02-11 16:31:36 +0530

    Added styled components package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Callstack from './components/Callstack/Callstack';
+import Callstack from './components/callstack/Callstack';
+import WebApi from './components/web-api/WebApi';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<Callstack />
+				<WebApi />
 			</div>
 		);
 	}
--- src/components/Callstack/Callstack.js
+++ src/components/Callstack/Callstack.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from './node_modules/react';
 import './Callstack.styles.css';
 
 class Callstack extends Component {
