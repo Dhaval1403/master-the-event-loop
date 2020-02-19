ccd51d589cdd6bf65dd90aacabdae68c53bb8a32
faizhameed <faizftb@gmail.com>
2020-02-19 22:26:26 +0530

    Added outclick to close the modal and some styling to scroll of the modal. Rest is coming

--- src/components/Help/Help.jsx
+++ src/components/Help/Help.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
-import './Help.styles.css';
-import { connect } from 'react-redux';
-import { toggleHelp } from '../../redux/helpToggle/helpToggle.actions';
-import helpImage from '../../assets/master-the-event-loop.png';
-import gif_1 from '../../assets/animation1.gif';
-import gif_2 from '../../assets/animation2.gif';
-import gif_3 from '../../assets/animation3.gif';
-import gif_4 from '../../assets/animation4.gif';
-import gif_5 from '../../assets/animation5.gif';
-import playButton from '../../assets/play.png';
+import React, { useState } from 'react'
+import './Help.styles.css'
+import { connect } from 'react-redux'
+import { toggleHelp } from '../../redux/helpToggle/helpToggle.actions'
+import helpImage from '../../assets/master-the-event-loop.png'
+import gif_1 from '../../assets/animation1.gif'
+import gif_2 from '../../assets/animation2.gif'
+import gif_3 from '../../assets/animation3.gif'
+import gif_4 from '../../assets/animation4.gif'
+import gif_5 from '../../assets/animation5.gif'
+import playButton from '../../assets/play.png'
 
 const Help = ({ isHelp, toggleHelp }) => {
+	const currentTarget = (e) => {
+		if (e.currentTarget === e.target) {
+			toggleHelp()
+		} else {
+			return
+		}
+	}
 	return (
 		<React.Fragment>
 			{isHelp ? (
-				<div className="modal-container">
-					<div className="modal-body">
+				<div id="mdc" onClick={currentTarget} className="modal-container">
+					<div id="mdb" className="modal-body">
 						<div className="modal-header">
 							<h2>Master the Javascript Event Loop</h2>
 
@@ -26,39 +33,40 @@ const Help = ({ isHelp, toggleHelp }) => {
 
 						<div className="modal-content">
 							<p>
-								JavaScript is single-threaded: only one task can run at a time. Usually that’s no
-								big deal, but now imagine you’re running a task which takes 30 seconds.. Ya.. During
-								that task we’re waiting for 30 seconds before anything else can happen (JavaScript
-								runs on the browser’s main thread by default, so the entire UI is stuck) It’s 2020,
-								no one wants a slow, unresponsive website.
+								JavaScript is single-threaded: only one task can run at a time. Usually that’s no big deal,
+								but now imagine you’re running a task which takes 30 seconds.. Ya.. During that task we’re
+								waiting for 30 seconds before anything else can happen (JavaScript runs on the browser’s
+								main thread by default, so the entire UI is stuck) It’s 2020, no one wants a slow,
+								unresponsive website.
 							</p>
 
 							<p>
-								This project is based on how the Javascript event loop works under the hood. It is
-								the visual representation which shows you that how Javascript works internally. What exactly happens
-								when javascript encounters something like 'setTimeout', 'setInterval' or 'fetch'? How
-								Javascript deals with this asynchronous code because Javascript is a single threaded
-								language, it means that it can handle one thing at a time. Don't worry, we have
-								tried our best to explain this topic in the easiest way possible with some cool
-								animations & visuals. To get started write some code in the 'Code Editor' & hit that
+								This project is based on how the Javascript event loop works under the hood. It is the
+								visual representation which shows you that how Javascript works internally. What exactly
+								happens when javascript encounters something like 'setTimeout', 'setInterval' or 'fetch'?
+								How Javascript deals with this asynchronous code because Javascript is a single threaded
+								language, it means that it can handle one thing at a time. Don't worry, we have tried our
+								best to explain this topic in the easiest way possible with some cool animations & visuals.
+								To get started write some code in the 'Code Editor' & hit that
 								<img className="play-button" src={playButton} alt="Play Button" />
-								button. Woohooo!! See the magic that javascript does for us behind the scenes.
-								Excited? Let's get started
+								button. Woohooo!! See the magic that javascript does for us behind the scenes. Excited?
+								Let's get started
 							</p>
 						</div>
-						
+
 						<div className="resources">
 							<p>
-								Below are the some really cool animations which will show you how the event loop works in 5 steps. 
+								Below are the some really cool animations which will show you how the event loop works in 5
+								steps.
 							</p>
-							
+
 							<span className="resource-title">Animations</span>
 							<img className="resource-item" src={gif_1} alt="animations" />
 							<img className="resource-item" src={gif_2} alt="animations" />
 							<img className="resource-item" src={gif_3} alt="animations" />
 							<img className="resource-item" src={gif_4} alt="animations" />
 							<img className="resource-item" src={gif_5} alt="animations" />
-								
+
 							<p>
 								(Source of animations: &nbsp;
 								<a
@@ -71,17 +79,16 @@ const Help = ({ isHelp, toggleHelp }) => {
 						</div>
 
 						<div className="resources">
-							<p>Below attached are some extra resources for you if you want to dive deep into the
-								topic. It includes some images, articles and youtube videos.</p>
-							
+							<p>
+								Below attached are some extra resources for you if you want to dive deep into the topic. It
+								includes some images, articles and youtube videos.
+							</p>
+
 							<span className="resource-title">Images</span>
 							<img className="resource-item" src={helpImage} alt="master the event loop" />
 
 							<span className="resource-title">Videos</span>
-							<iframe
-								className="resource-item"
-								src="https://www.youtube.com/embed/hGSHfObcVf4"
-							></iframe>
+							<iframe className="resource-item" src="https://www.youtube.com/embed/hGSHfObcVf4"></iframe>
 
 							<span className="resource-title">Articles</span>
 							<p>
@@ -108,15 +115,15 @@ const Help = ({ isHelp, toggleHelp }) => {
 				</div>
 			) : null}
 		</React.Fragment>
-	);
-};
+	)
+}
 
 const mapStateToProps = ({ helpReducer: { isHelp } }) => ({
 	isHelp,
-});
+})
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
 	toggleHelp: () => dispatch(toggleHelp()),
-});
+})
 
-export default connect(mapStateToProps, mapDispatchToProps)(Help);
+export default connect(mapStateToProps, mapDispatchToProps)(Help)
