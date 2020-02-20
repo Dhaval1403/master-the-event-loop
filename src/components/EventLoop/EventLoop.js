4e4bcc1bc8821a01fa8452764d8dc2695b1729fe
tubacatal <tubacatal>
2020-02-20 23:06:21 +0300

    naming conflict and event loop component as a class component

--- src/components/EventLoop/EventLoop.js
+++ src/components/EventLoop/EventLoop.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react'
-import './EventLoop.styles.css'
+import React, {Component} from 'react';
+import './EventLoop.styles.css';
 
-const EventLoop = (props) => {
-	const [spin, setSpin] = useState(false)
+class EventLoop extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			spin: false
+		};
+	}
 
-	setTimeout(() => {
-		setSpin(true)
-	}, 1000)
+	render() {
 
-	return (
-		<div className={'arrow ' + (spin ? 'spin' : '')}>
-			<div className="circle" />
-			<div className="triangle" />
-		</div>
-	)
+		setTimeout(() => {
+			this.setState({
+				spin: true
+			});
+		}, 1000);
+
+		return (
+			<div className={'arrow ' + (this.state.spin ? 'spin' : '')}>
+				<div className="circle"/>
+				<div className="triangle"/>
+			</div>
+		)
+	}
 }
 
-export default EventLoop
+export default EventLoop;
