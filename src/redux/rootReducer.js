87714f876bc79dafcd458efdbe25588eba9386f1
r4pt0s <r4pt0s@protonmail.com>
2020-02-21 12:48:10 +0100

    add Queue data structure as class, add redux callstack logic

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,15 @@
 import { combineReducers } from 'redux'
 import { helpReducer } from './helpToggle/helpToggle.reducer'
+import { consoleReducer } from './Console/Console.reducer'
+import { callstackReducer } from './callstack/callstack.reducer'
+import { webApiReducer } from './webApi/webApi.reducer'
 
 // import all reducers in this file and add them with a flag to the combineReducer method by adding it to the object
 // reducerName : reducer;
 
 export default combineReducers({
 	helpReducer: helpReducer,
+	consoleReducer: consoleReducer,
+	callstack: callstackReducer,
+	webApi: webApiReducer,
 })
