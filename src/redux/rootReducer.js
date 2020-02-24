f5944cefe34b773941365ed50f4f33d9afb5b3cc
rahulzura <rahuldesune@gmail.com>
2020-02-24 23:52:45 +0530

    minor removed unneeded files

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -4,7 +4,6 @@ import { consoleReducer } from './Console/Console.reducer'
 import { callstackReducer } from './callstack/callstack.reducer'
 import { callbackQueueReducer } from './callbackQueue/callbackQueue.reducer'
 import { webApiReducer } from './WebApiRedux/webApi.reducer'
-import { eventLoopReducer } from './eventLoop/eventLoop.reducer'
 
 // import all reducers in this file and add them with a flag to the combineReducer method by adding it to the object
 // reducerName : reducer;
@@ -15,5 +14,4 @@ export default combineReducers({
 	callstack: callstackReducer,
 	callbackQueue: callbackQueueReducer,
 	webApiReducer: webApiReducer,
-	eventLoop: eventLoopReducer,
 })
