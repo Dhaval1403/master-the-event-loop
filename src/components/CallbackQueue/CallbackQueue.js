749e0db899d5cfb9d3b5dfbc14eeb68d391d1078
rahulzura <rahuldesune@gmail.com>
2020-02-24 23:50:30 +0530

    minor callbackqueue li error fix

--- src/components/CallbackQueue/CallbackQueue.js
+++ src/components/CallbackQueue/CallbackQueue.js
@@ -36,9 +36,7 @@ class CallbackQueue extends Component {
 				<Box borderTop={1} borderStyle="solid" color="colorBlue" />
 
 				<Box display="flex" justifyContent="center" m="20px 0">
-					<List>
-						<ListItem>{values}</ListItem>
-					</List>
+					<List>{values}</List>
 				</Box>
 			</ConsoleBox>
 		)
